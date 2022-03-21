8a57d525201b70374c2e6cebfa12c65e2edfc720
Mohammad Afzali <info@afzaliwp.com>
2022-03-21 12:53:22 +0330

    Change the way to save start and end guaranty date

--- functions.php
+++ functions.php
@@ -116,27 +116,31 @@ function fi_gregorian_to_jalali( $date, $separator = '-' ) {
 }
 
 function fi_save_public_form_data( $data ) {
-	$code = $data['guaranty_code'];
+	$code        = $data['guaranty_code'];
 	$code_exists = fi_get_code( $code );
 
-	if (!$code_exists ){
+	if ( ! $code_exists ) {
 		return false;
 	}
 
-	if ( !empty( $code_exists->customer ) || !empty( $code_exists->installer ) ) {
+	if ( ! empty( $code_exists->customer ) || ! empty( $code_exists->installer ) ) {
 		return false;
 	}
 
-	$name = $data['customer_name'];
+	$name     = $data['customer_name'];
 	$phone    = $data['customer_phone'];
 	$customer = [ 'name' => $name, 'phone' => $phone ];
 	global $wpdb;
 	$tp = $wpdb->prefix;
 
 	return $wpdb->update( $tp . 'fi_guaranty',
-		[ 'customer' => maybe_serialize( $customer ) ],
+		[
+			'customer'   => maybe_serialize( $customer ),
+			'started_at' => wp_date( 'Y-m-d H:i:s' ),
+			'ended_at'   => wp_date( 'Y-m-d H:i:s', strtotime( '+2 years' ) )
+		],
 		[ 'type' => CUSTOMER, 'code' => $code ],
-		[ '%s' ],
+		[ '%s', '%s', '%s' ],
 		[ '%d', '%s' ]
 	);
 }
@@ -151,33 +155,14 @@ function fi_get_inquiry_result( $code ) {
 function fi_save_installer_form_data( $code ) {
 	$code_exists = fi_get_code( $code );
 
-	if (!$code_exists ){
+	if ( ! $code_exists ) {
 		return false;
 	}
 
-	if ( !empty( $code_exists->customer ) || !empty( $code_exists->installer ) ) {
+	if ( ! empty( $code_exists->customer ) || ! empty( $code_exists->installer ) ) {
 		return false;
 	}
 
-	$user      = wp_get_current_user();
-	$name      = $user->display_name;
-	$user_id   = $user->ID;
-	$phone     = get_user_meta( get_current_user_id(), 'billing_phone', true );
-	$installer = [ 'id' => $user_id, 'name' => $name, 'phone' => $phone ];
-
-	global $wpdb;
-	$tp = $wpdb->prefix;
-
-	return $wpdb->update( $tp . 'fi_guaranty',
-		[ 'installer' => maybe_serialize( $installer ) ],
-		[ 'type' => INSTALLER, 'code' => $code ],
-		[ '%s' ],
-		[ '%d', '%s' ]
-	);
-}
-
-function fi_save_guaranty_request( $data ) {
-	$code      = $data['guaranty_code'];
 	$user      = wp_get_current_user();
 	$name      = $user->display_name;
 	$user_id   = $user->ID;
@@ -190,12 +175,48 @@ function fi_save_guaranty_request( $data ) {
 	return $wpdb->update( $tp . 'fi_guaranty',
 		[
 			'installer'  => maybe_serialize( $installer ),
-			'request'    => $data['explanation'],
 			'started_at' => wp_date( 'Y-m-d H:i:s' ),
 			'ended_at'   => wp_date( 'Y-m-d H:i:s', strtotime( '+2 years' ) )
 		],
+		[ 'type' => INSTALLER, 'code' => $code ],
+		[ '%s', '%s', '%s' ],
+		[ '%d', '%s' ]
+	);
+}
+
+function fi_save_guaranty_request( $data ) {
+	$code = $data['guaranty_code'];
+
+	$code_exists = fi_get_code( $code );
+
+	if ( ! $code_exists ) {
+		return false;
+	}
+
+	if ( empty( $code_exists->customer ) || empty( $code_exists->installer ) ) {
+		return false;
+	}
+
+	if (wp_date('Y-m-d') > $code_exists['ended_at']) {
+		return false;
+	}
+
+	$user      = wp_get_current_user();
+	$name      = $user->display_name;
+	$user_id   = $user->ID;
+	$phone     = get_user_meta( get_current_user_id(), 'billing_phone', true );
+	$installer = [ 'id' => $user_id, 'name' => $name, 'phone' => $phone ];
+
+	global $wpdb;
+	$tp = $wpdb->prefix;
+
+	return $wpdb->update( $tp . 'fi_guaranty',
+		[
+			'installer' => maybe_serialize( $installer ),
+			'request'   => $data['explanation']
+		],
 		[ 'type' => INSTALLER, 'code' => $code, ],
-		[ '%s', '%s', '%s', '%s' ],
+		[ '%s', '%s' ],
 		[ '%d', '%s' ]
 	);
 }
